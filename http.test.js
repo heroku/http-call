06d42396d9ab4ff15ba948448d8c29ef9483ae9f
Jeff Dickey <216188+jdxcode@users.noreply.github.com>
2017-02-19 12:03:03 -0800

    jesting

--- http.test.js
+++ http.test.js
@@ -1,30 +1,32 @@
-/* globals test expect */
+/* globals test expect beforeAll */
 const util = require('util')
 const http = require('.')
 const nock = require('nock')
 
-function renderHeaders (headers) {
-  return Object.keys(headers).map(key => {
-    let value = key.toUpperCase() === 'AUTHORIZATION' ? 'REDACTED' : headers[key]
-    return '    ' + key + '=' + value
-  }).join('\n')
-}
+nock('https://api.heroku.com')
 
-function debugRequest (options) {
-  // console.log(`--> ${options.method} ${options.host}${options.path}`)
-  // console.log(renderHeaders(options.headers))
-  // if (body) console.log(`--- BODY\n${util.inspect(body)}\n---`)
-  return Promise.resolve(options)
-}
+test('makes a GET request', async () => {
+  function renderHeaders (headers) {
+    return Object.keys(headers).map(key => {
+      let value = key.toUpperCase() === 'AUTHORIZATION' ? 'REDACTED' : headers[key]
+      return '    ' + key + '=' + value
+    }).join('\n')
+  }
 
-function debugResponse ({response, body}) {
-  let url = `${response.req._headers.host}${response.req.path}`
-  // console.log(`<-- ${response.req.method} ${url} ${response.statusCode}`)
-  // console.log(renderHeaders(response.headers))
-  // console.log(`--- BODY\n${util.inspect(body)}\n---`)
-}
+  function debugRequest (options) {
+    // console.log(`--> ${options.method} ${options.host}${options.path}`)
+    // console.log(renderHeaders(options.headers))
+    // if (body) console.log(`--- BODY\n${util.inspect(body)}\n---`)
+    return Promise.resolve(options)
+  }
 
-test('makes a GET request', async function () {
+  function debugResponse ({response, body}) {
+    let url = `${response.req._headers.host}${response.req.path}`
+    // console.log(`<-- ${response.req.method} ${url} ${response.statusCode}`)
+    // console.log(renderHeaders(response.headers))
+    // console.log(`--- BODY\n${util.inspect(body)}\n---`)
+  }
+
   let rsp = await http.get('https://api.heroku.com', {
     debug: 2,
     requestMiddleware: debugRequest,
