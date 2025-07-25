8784bd2da5da50f464069aad5ed34b785eb5e750
Santiago Bosio <santiago.bosio@gmail.com>
2025-07-25 17:38:17 -0300

    chore: Fix reported vulnerabilities (#69)

--- jest.config.js
+++ jest.config.js
@@ -2,11 +2,8 @@ module.exports = {
   moduleFileExtensions: ['ts', 'js'],
   testMatch: ['**/*.test.ts'],
   transform: {
-    '^.+.ts$': '<rootDir>/node_modules/ts-jest/preprocessor.js',
-  },
-  globals: {
-    'ts-jest': {
-      skipBabel: true,
-    },
+    '^.+.ts$': ['ts-jest', {
+      tsconfig: 'tsconfig.json',
+    }],
   },
 }
